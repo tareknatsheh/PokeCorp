9d0f1ff9c53c72ccbd96b5968ed58b42b7aa5479
tareknatsheh <tarek.o.natsheh@gmail.com>
2024-05-26 13:30:33 +0300

    app.py: fixed bugs and function namings

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from fastapi import FastAPI
 from Model.db import db
 
-app = FastAPI
+app = FastAPI()
 
-@app.get("/")
+@app.get("/{poke_type}")
 def find_pokemon_by_type(poke_type: str):
-    # pokemon_names = find_pokemon_by_type(poke_type)
     pokemon_names = db.findByType(poke_type)
     return {"pokemon_names": pokemon_names}
 
 
 @app.get("/{pokemon_name}")
 def find_trainers_by_pokemon(pokemon_name: str):
-    trainer_names = db.findByOwner(pokemon_name)
+    trainer_names = db.findOwners(pokemon_name)
     return {"trainer_names": trainer_names}
 
 
 @app.get("/{trainer_name}")
 def find_pokemons_by_trainer(trainer_name: str):
-  
-  pokemon_names = db.PokemonsOfTrainer(trainer_name)
+  pokemon_names = db.findPokemonsOfOwner(trainer_name)
   return {"pokemon_names": pokemon_names}
 
