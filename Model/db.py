90b2654e60f50c44a604f5abd161607aa5a42be9
tareknatsheh <tarek.o.natsheh@gmail.com>
2024-05-26 12:15:38 +0300

    built placeholders for exer. functions

--- Model/db.py
+++ Model/db.py
@@ -1,9 +1,16 @@
 class Database:
     def __init__(self):
+        print("Initializing the DB")
         pass
 
-    def findByType(self, type: str):
-        pass
+    def findByType(self, type: str) -> list[str]:
+        return []
+    
+    def findOwners(self, pokemon_name: str) -> list[str]:
+        return []
+    
+    def findPokemonsOfOwner(self, owner_name) -> list[str]:
+        return []
 
 
 db = Database()
