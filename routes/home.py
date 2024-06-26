a69e678034983e8d081e8dcc6a80e2a800c2c8f9
tareknatsheh <tarek.o.natsheh@gmail.com>
2024-06-27 02:32:46 +0300

    organized the frontend into prod and dev

--- routes/home.py
+++ routes/home.py
@@ -2,6 +2,7 @@ import os
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from routes.utils.routes_error_handler import handle_route_errors
+from config import frontend_folder_name
 
 router = APIRouter()
 
@@ -9,7 +10,7 @@ router = APIRouter()
 @router.get("/", response_class=HTMLResponse, include_in_schema=False)
 @handle_route_errors
 def read_root():
-    html_file_path = os.path.join("public", "index.html")
+    html_file_path = os.path.join(frontend_folder_name, "index.html")
     with open(html_file_path, 'r') as html_file:
         return html_file.read()
 
